7ee463e3554486ef9afedfd090cc167e3f596419
MuhammadHashir81 <MuhammadHashir8181@gmail.com>
2023-08-26 20:26:22 +0500

    first commit

--- tabbed-app/src/TabContent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TabContent = ({ isActive, children }) => {
-  return isActive ? <div className="tab-content">{children}</div> : null;
-};
-
-export default TabContent;
